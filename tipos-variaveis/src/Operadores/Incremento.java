8f25943331844fab0fceaf09b8717850a958ecbe
cunhacinthia <cunhacinthia69@gmail.com>
2024-10-28 21:51:51 -0300

    Criação da classe incremento

---
+++ tipos-variaveis/src/Operadores/Incremento.java
@@ -0,0 +1,18 @@
+package operadores;
+
+public class Incremento {
+    
+    public static void main(String[] args) {
+        //A incrementação está associada a recorrencia de atribuição de um mesmo valor
+        int numero = 5;
+        //x repetição
+        numero++; // é a mesma representação de numero + 1
+
+        System.out.println(numero);
+
+        numero--;
+
+        System.out.println(numero);
+
+    }
+}
